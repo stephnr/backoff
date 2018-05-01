111442a9e6a91414293c3d907be846d2f618a68c
Stephen Rodriguez <steprodriguez10@gmail.com>
2018-05-01 19:19:10 +0200

    add doc file

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package backoff is a service that implements a variety of backoff algorithms to provide Optimistic Concurrency Control.
+// This package currently supports the following backoff algorithms:
+//
+// - Exponential Backoff
+//
+package backoff
